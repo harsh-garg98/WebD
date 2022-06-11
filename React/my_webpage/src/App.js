5536d533727b8fef0e3a5b6376462c16365fd519
TheNightBaron <harshgarg98@hotmail.com>
2022-06-11 19:56:11 +0530

    trying to make bulk changes

--- React/my_webpage/src/App.js
+++ React/my_webpage/src/App.js
@@ -1,32 +1,27 @@
-import logo from './logo.svg';
-import image from './components/img2.jpeg'
-import image2 from './components/img1.jpeg'
-import acn from './components/acn.png'
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 import './App.css';
-import React, { useState } from 'react';
-import page_header from './components/page_header.js';
+import PageHeader from './components/PageHeader.js';
+import PageBody from './components/PageBody.js';
+import TopMenu from './components/TopMenu';
 
 function App() {
-  
+
   return (
     <div className="App">
-    <header className="app-header">
-        <h3 className="header-first-sentence">Hey, I'm</h3>
-        <h1 className="header-second-sentence">HARSH GARG</h1>
-    </header>
-      <h1 className="my-journey">My journey</h1>
-      <div className="image-and-details">
-      <img className="journey-image" src={acn}  alt="Accenture" />
-            <div className = "company-details">
-              <p>Accenture, India</p>
-              <p>Data Science Analyst</p>
-            </div>
-            </div>
+      <TopMenu />
+      <div id="body-start">
+        <PageHeader />
+        <PageBody />
+      </div>
     </div>
   );
 }
 
 export default App;
-
-
- 
