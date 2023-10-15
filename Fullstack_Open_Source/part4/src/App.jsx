4445288875849e75a3e9cc229d569d4e1ef464e3
TheNightBaron <harshgarg98@hotmail.com>
2023-10-15 21:42:14 +0530

    Minor tweaks

--- Fullstack_Open_Source/part4/src/App.jsx
+++ Fullstack_Open_Source/part4/src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("a new note...");
   const [showAll, setShowAll] = useState(true);
-  const [errorMessage, setErrorMessage] = useState("some error happened...");
+  const [errorMessage, setErrorMessage] = useState(null);
 
   const toggleImportanceOf = (id) => {
     const url = `http://localhost:3001/notes/${id}`;
