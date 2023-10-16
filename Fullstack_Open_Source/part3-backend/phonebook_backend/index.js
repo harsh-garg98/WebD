664d610a49a263f7d9c8e571c0c8d87e2c25bda8
TheNightBaron <harshgarg98@hotmail.com>
2023-10-16 16:18:18 +0530

    uibuild

--- Fullstack_Open_Source/part3-backend/phonebook_backend/index.js
+++ Fullstack_Open_Source/part3-backend/phonebook_backend/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 var morgan = require("morgan");
+const cors = require("cors");
 const app = express();
+
 app.use(express.json());
+app.use(cors());
 
 const requestLogger = (request, response, next) => {
   console.log("Method: ", request.method);
