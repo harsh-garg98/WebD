1c3f4f193febc6f3bca7f8c9ec25ddb3eba4bdb1
TheNightBaron <harshgarg98@hotmail.com>
2023-10-16 16:24:09 +0530

    uibuild

--- Fullstack_Open_Source/part3-backend/phonebook_backend/index.js
+++ Fullstack_Open_Source/part3-backend/phonebook_backend/index.js
@@ -5,6 +5,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static("dist"));
 
 const requestLogger = (request, response, next) => {
   console.log("Method: ", request.method);
@@ -115,7 +116,7 @@ app.post("/api/persons", (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
